019b1e633c1ad73301ad607b8864dbd28135a22b
Jordancsb <jcaindoor@gmail.com>
2022-03-23 20:05:46 -0300

    'Upload projeto'

---
+++ src/main/java/one/digitalinnovation/apipessoal/ApipessoalApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.apipessoal;
+
+import org.springframework.boot.Spring.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipessoalApplication.class, args);
+	}
+
+}
